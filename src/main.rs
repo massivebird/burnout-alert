20517cfed6d2c76d152e946605337a817a0c6ea7
massivebird <gdrakemail@gmail.com>
2024-08-09 11:05:29 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let phrases = vec![
+        "Nice airtime! +200pts",
+        "BIG DAMAGE! +1,000pts",
+        "Utter destruction! +300pts",
+    ];
+
+    let rng = rand::thread_rng();
+
+    println!("Hello, world!");
+}
