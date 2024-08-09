50cc58b890f76c3dd1f9ffd4c89e6b7d63bd2878
massivebird <gdrakemail@gmail.com>
2024-08-09 11:17:10 -0400

    feat: animated print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::io::{self, Write};
+use std::thread;
+use std::time::Duration;
+
 use rand::seq::SliceRandom;
 
 fn main() {
@@ -7,5 +11,14 @@ fn main() {
         "Utter destruction! +300pts",
     ];
 
-    println!("{}", phrases.choose(&mut rand::thread_rng()).unwrap());
+    // println!("{}", phrases.choose(&mut rand::thread_rng()).unwrap());
+    animated_print(phrases.choose(&mut rand::thread_rng()).unwrap());
+}
+
+fn animated_print(str: &str) {
+    for char in str.chars() {
+        print!("{char}");
+        io::stdout().flush().unwrap();
+        thread::sleep(Duration::from_millis(30))
+    }
 }
